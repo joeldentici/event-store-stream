a65adeabc8f2dc4cdbcf56b5f5265c09c51f5922
Joel Dentici <joeldentici@cox.net>
2017-07-26 17:48:10 -0700

    Removed duplicate code

--- src/util.js
+++ src/util.js
@@ -26,3 +26,48 @@ exports.simplifyEvent = function(event) {
 		});
 	}
 }
+
+/**
+ *	addDomainInfo :: Event -> DomainObjectEvent
+ *
+ *	Adds information about the domain object that
+ *	an event is tied to.
+ */
+const addDomainInfo = exports.addDomainInfo = function(event) {
+	const streamId = event.eventStreamId;
+	const sepAt = streamId.indexOf('-');
+	const domainObjectType = streamId.substring(0, sepAt);
+	const domainObjectId = streamId.substring(sepAt + 1);
+
+	return Object.assign({}, event, {
+		domainObjectType,
+		domainObjectId
+	});
+}
+
+/**
+ *	interested :: [string] -> Event -> bool
+ *
+ *	Creates a filter predicate for the specified event types
+ */
+const interested = exports.interested = function(eventTypes) {
+	const types = new Set(eventTypes);
+	return function(event) {
+		return types.has(event.eventType);
+	}
+}
+
+/**
+ *	getInterestingEvents :: (Observable Event, [string], int) -> Observable [Event]
+ *
+ *	Map an event stream to a stream of event windows, containing events
+ *	of the specified types. Windows are buffered for the provided throttle
+ *	time.
+ */
+exports.getInterestingEvents = function(stream$, types, throttle) {
+	return stream$
+			.filter(interested(types))
+			.map(addDomainInfo)
+			.buffer(Rx.Observable.interval(throttle))
+			.filter(x => x.length)
+}
